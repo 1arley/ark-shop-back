f0fbf2d922be61b9663e156ad5c35311f1311965
1arley <arthuriarley323@gmail.com>
2026-05-12 07:15:51 -0300

    feat: add module-alias dependency and configure path aliases in index.js

--- api/index.js
+++ api/index.js
@@ -1,6 +1,20 @@
+// 1. Força o compilador da Vercel a incluir os pacotes dinâmicos do NestJS no build
+require('@nestjs/axios');
+require('axios');
+
+// 2. Mapeia os path aliases do TypeScript para funcionarem no Node.js em produção
+const path = require('path');
+const moduleAlias = require('module-alias');
+
+moduleAlias.addAliases({
+  '@utils': path.resolve(__dirname, '../dist/utils'),
+  '@': path.resolve(__dirname, '../dist'),
+  '~': path.resolve(__dirname, '../dist'),
+  '@test': path.resolve(__dirname, '../test')
+});
+
 // Vercel Serverless Function entrypoint
 // Uses compiled dist/ output from nest build
-
 const { createApp } = require('../dist/main');
 
 let cachedApp;
