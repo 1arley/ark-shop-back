bac33b0beacda95627a77af9c8780db249bb21a6
1arley <arthuriarley323@gmail.com>
2026-05-14 16:12:22 -0300

    fix: remove duplicate commands from product imageUrl migration

--- prisma/migrations/20260513135653_add_product_imageurl/migration.sql
+++ prisma/migrations/20260513135653_add_product_imageurl/migration.sql
@@ -1,6 +1,2 @@
 -- AlterTable
 ALTER TABLE "Product" ADD COLUMN "imageUrl" TEXT;
-
--- AlterTable
-ALTER TABLE "Seller" ADD COLUMN "pixKey" TEXT;
-CREATE UNIQUE INDEX "Seller_asaasAccountId_key" ON "Seller"("asaasAccountId");
